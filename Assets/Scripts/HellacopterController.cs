b6b211bf795febabceee9dc777d149c6b3916f15
Salustiano Oliveira <salultura@gmail.com>
2018-03-28 21:00:53 -0300

    Criando o Projeto Hellacopter
    - Adicionando os recursos ao projeto;
    - Cena base configurada;
    - Criado os prefabs do hellacopters;
    - Adicionando o script de controle do hellacopter;
    - adicionado o script de movimentação dos obstaculos;

---
+++ Assets/Scripts/HellacopterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellacopterController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float alturaDoPulo = 10;
+    public float forcaDoFreio = -5;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rb.AddForce(Vector2.up * alturaDoPulo, ForceMode2D.Impulse);
+        }else if (Input.GetButtonDown("Fire2"))
+        {
+            rb.AddForce(Vector2.up * forcaDoFreio, ForceMode2D.Impulse);
+        }
+
+    }
+}
